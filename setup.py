717601f5c2d8457b15ff5978c522af4cc5aca2f2
Baptiste Busch <baptiste.busch@gmail.com>
2016-09-04 11:30:01 +0200

    Create package architechture

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PokeLife',
+      version='1.0',
+      description='Pokemon AI battle test',
+      author='Baptiste Busch',
+      author_email='baptiste.busch@gmail.com',
+      url='https://github.com/buschbapti/PokeLife',
+      packages=['pokelife'],
+      package_dir={'': 'src'}
+     )
